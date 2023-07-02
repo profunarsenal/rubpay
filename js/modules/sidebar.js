283225f073891b0e24b237d683312344329a3664
Дмитрий Ширяев <dshiryaev@srt-llc.ru>
2023-07-02 11:33:35 +0400

    123

--- js/modules/sidebar.js
+++ js/modules/sidebar.js
@@ -1,6 +1,7 @@
 const sidebar = () => {
     const sidebar = document.querySelector(".sidebar");
     const container = document.querySelector(".container");
+    const footer = document.querySelector(".footer");
     const buttonCollapseSidebar = document.querySelector(".sidebar__collapse-button");
     const tooltip = buttonCollapseSidebar.querySelector(".tooltip");
 
@@ -8,6 +9,7 @@ const sidebar = () => {
         buttonCollapseSidebar.addEventListener("click", () => {
             sidebar.classList.toggle("collapse");
             container.classList.toggle("collapse");
+            footer.classList.toggle("collapse");
             if (sidebar.classList.contains("collapse")) {
                 tooltip.textContent = "Развернуть";
             } else {
